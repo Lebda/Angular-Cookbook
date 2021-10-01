329fc72e046dd9c23a953a61e15f8763de277b88
Lebda <lebduska.jiri@seznam.cz>
2021-10-01 11:21:20 +0200

    ch 1 ex 01

--- chapter01/start_here/cc-inputs-outputs/src/app/app.component.ts
+++ chapter01/start_here/cc-inputs-outputs/src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  public notificationsCount = 10;
+
+  public updateNotificationsCount(count: number): void {
+    this.notificationsCount = count;
+  }
 }
