4916f8ddfc7550551aa60658a84b0453154b7268
Lebda <lebduska.jiri@seznam.cz>
2021-10-03 13:44:56 +0200

    ch 05 ex 01

--- chapter05/start_here/rxjs-operators-instance-methods/src/app/app.component.ts
+++ chapter05/start_here/rxjs-operators-instance-methods/src/app/app.component.ts
@@ -1,30 +1,38 @@
 import { Component } from '@angular/core';
 import { interval, Subscription } from 'rxjs';
+import { map, merge } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   subscription: Subscription = null;
   inputStreamData = ['john wick', 'inception', 'interstellar'];
   outputStreamData = [];
+  cartoonsStreamData = ['thunder cats', 'Dragon Ball Z', 'Ninja Turtles'];
 
-  ngOnInit() {
-  }
+  public ngOnInit(): void {}
 
-  startStream() {
-    const streamSource = interval(1500);
-    this.subscription = streamSource.subscribe(input => {
+  public startStream(): void {
+    const cartoonStreamSource = interval(1000).pipe(
+      map((output) => output % this.cartoonsStreamData.length),
+      map((index) => this.cartoonsStreamData[index])
+    );
+
+    const streamSource = interval(1000).pipe(
+      map((output) => output % this.inputStreamData.length),
+      map((index) => this.inputStreamData[index]),
+      merge(cartoonStreamSource)
+    );
+    this.subscription = streamSource.subscribe((input) => {
       this.outputStreamData.push(input);
     });
   }
 
-  stopStream() {
+  public stopStream(): void {
     this.subscription.unsubscribe();
     this.subscription = null;
   }
-
-
 }
