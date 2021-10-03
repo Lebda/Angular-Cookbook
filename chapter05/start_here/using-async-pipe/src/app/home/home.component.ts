cb4126a7a834eb70d414f500b8246705a5d3865f
Lebda <lebduska.jiri@seznam.cz>
2021-10-03 14:18:55 +0200

    ch 05 async pipe

--- chapter05/start_here/using-async-pipe/src/app/home/home.component.ts
+++ chapter05/start_here/using-async-pipe/src/app/home/home.component.ts
@@ -1,58 +1,75 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { merge, Observable } from 'rxjs';
 import { interval } from 'rxjs/internal/observable/interval';
 import { Subscription } from 'rxjs/internal/Subscription';
-import { takeWhile } from 'rxjs/operators';
+import { map, takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit, OnDestroy {
   isComponentAlive: boolean;
   subscription: Subscription = null;
   inputStreamData = ['john wick', 'inception', 'interstellar'];
   outputStreamData = [];
+  streamsOutput$: Observable<string[]>;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  public ngOnInit(): void {
     this.startStream();
   }
 
-  ngOnDestroy() {
+  public ngOnDestroy(): void {
     this.stopStream();
   }
 
-  startStream() {
+  // public startStream(): void {
+  //   this.isComponentAlive = true;
+  //   const streamSource = interval(1500);
+  //   const secondStreamSource = interval(3000);
+  //   const fastestStreamSource = interval(500);
+  //   streamSource
+  //     .pipe(takeWhile(() => !!this.isComponentAlive))
+  //     .subscribe((input) => {
+  //       this.outputStreamData.push(input);
+  //       console.log('stream output', input);
+  //     });
+  //   secondStreamSource
+  //     .pipe(takeWhile(() => !!this.isComponentAlive))
+  //     .subscribe((input) => {
+  //       this.outputStreamData.push(input);
+  //       console.log('second stream output', input);
+  //     });
+  //   fastestStreamSource
+  //     .pipe(takeWhile(() => !!this.isComponentAlive))
+  //     .subscribe((input) => {
+  //       this.outputStreamData.push(input);
+  //       console.log('fastest stream output', input);
+  //     });
+  // }
+
+  public startStream(): void {
     this.isComponentAlive = true;
     const streamSource = interval(1500);
     const secondStreamSource = interval(3000);
     const fastestStreamSource = interval(500);
-    streamSource
-      .pipe(
-        takeWhile(() => !!this.isComponentAlive)
-      ).subscribe(input => {
-      this.outputStreamData.push(input);
-      console.log('stream output', input)
-    });
-    secondStreamSource
-      .pipe(
-        takeWhile(() => !!this.isComponentAlive)
-      ).subscribe(input => {
-        this.outputStreamData.push(input);
-        console.log('second stream output', input)
-      });
-    fastestStreamSource
-      .pipe(
-        takeWhile(() => !!this.isComponentAlive)
-      ).subscribe(input => {
-        this.outputStreamData.push(input);
-        console.log('fastest stream output', input)
-      });
+    this.streamsOutput$ = merge(
+      streamSource,
+      secondStreamSource,
+      fastestStreamSource
+    ).pipe(
+      // takeWhile(() => !!this.isComponentAlive),
+      map((index) => {
+        console.log('index', index);
+        this.outputStreamData = [...this.outputStreamData, index];
+        return this.outputStreamData;
+      })
+    );
   }
 
-  stopStream() {
-    this.isComponentAlive = false;
+  public stopStream(): void {
+    // thist.isComponentAlive = false;
   }
 }
