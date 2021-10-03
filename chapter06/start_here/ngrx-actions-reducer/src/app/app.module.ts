6210a4f9ef7669eb19ed0007fa2607f2b84feb18
Lebda <lebduska.jiri@seznam.cz>
2021-10-03 18:21:26 +0200

    ch 06 ex 02

--- chapter06/start_here/ngrx-actions-reducer/src/app/app.module.ts
+++ chapter06/start_here/ngrx-actions-reducer/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import * as appStore from './store/app.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +17,9 @@ import * as appStore from './store/app.reducer';
     FormsModule,
     BrowserAnimationsModule,
     StoreModule.forRoot({ app: appStore.reducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
