9428a557a15219e838a0beb238d031cc277f55cb
Lebda <lebduska.jiri@seznam.cz>
2021-10-03 14:44:19 +0200

    ch 05 combine latests

--- chapter05/start_here/using-combinelatest-operator/src/app/home/home.component.ts
+++ chapter05/start_here/using-combinelatest-operator/src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { combineLatest, Observable } from 'rxjs';
 import { COLORS } from '../constants';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -10,51 +12,86 @@ import { COLORS } from '../constants';
 export class HomeComponent implements OnInit, OnDestroy {
   sizeOptions = [100, 200, 300, 400, 500, 600, 700];
   colorOptions = COLORS;
-  borderRadiusOptions = [4, 6, 8, 10, 12, 14, 16, 18, 20]
+  borderRadiusOptions = [4, 6, 8, 10, 12, 14, 16, 18, 20];
 
-  boxStyles: {
-    width: string,
-    height: string,
-    backgroundColor: string,
-    color: string
-    borderRadius: string
-  }
+  // boxStyles: {
+  //   width: string;
+  //   height: string;
+  //   backgroundColor: string;
+  //   color: string;
+  //   borderRadius: string;
+  // };
+  boxStyles$: Observable<{
+    width: string;
+    height: string;
+    backgroundColor: string;
+    color: string;
+    borderRadius: string;
+  }>;
 
   boxForm = new FormGroup({
     size: new FormControl(''),
     borderRadius: new FormControl(''),
     textColor: new FormControl(''),
     backgroundColor: new FormControl(''),
-  })
+  });
 
   constructor() {}
 
-  ngOnInit() {
-    this.boxForm.get('size').setValue(this.sizeOptions[0]);
-    this.boxForm.get('backgroundColor').setValue(this.colorOptions[0]);
-    this.boxForm.get('textColor').setValue(this.colorOptions[1]);
-    this.boxForm.get('borderRadius').setValue(this.borderRadiusOptions[0]);
-    this.applyChanges();
+  ngOnInit(): void {
+    // this.boxForm.get('size').setValue(this.sizeOptions[0]);
+    // this.boxForm.get('backgroundColor').setValue(this.colorOptions[0]);
+    // this.boxForm.get('textColor').setValue(this.colorOptions[1]);
+    // this.boxForm.get('borderRadius').setValue(this.borderRadiusOptions[0]);
+    // this.applyChanges();
+    this.listenToInputChanges();
   }
 
-  setBoxStyles(size, backgroundColor, color, borderRadius) {
-    this.boxStyles = {
-      width: `${size}px`,
-      height: `${size}px`,
-      backgroundColor,
-      color,
-      borderRadius: `${borderRadius}px`
-    }
+  listenToInputChanges(): void {
+    this.boxStyles$ = combineLatest([
+      this.boxForm
+        .get('size')
+        .valueChanges.pipe(startWith(this.sizeOptions[0])),
+      this.boxForm
+        .get('borderRadius')
+        .valueChanges.pipe(startWith(this.borderRadiusOptions[0])),
+      this.boxForm
+        .get('backgroundColor')
+        .valueChanges.pipe(startWith(this.colorOptions[1])),
+      this.boxForm
+        .get('textColor')
+        .valueChanges.pipe(startWith(this.colorOptions[0])),
+    ]).pipe(
+      map(([size, borderRadius, backgroundColor, textColor]) => {
+        return {
+          width: `${size}px`,
+          height: `${size}px`,
+          backgroundColor,
+          color: textColor,
+          borderRadius: `${borderRadius}px`,
+        };
+      })
+    );
   }
 
-  applyChanges() {
-    this.setBoxStyles(
-      this.boxForm.get('size').value,
-      this.boxForm.get('backgroundColor').value,
-      this.boxForm.get('textColor').value,
-      this.boxForm.get('borderRadius').value,
-    )
-  }
+  // setBoxStyles(size, backgroundColor, color, borderRadius): void {
+  //   this.boxStyles = {
+  //     width: `${size}px`,
+  //     height: `${size}px`,
+  //     backgroundColor,
+  //     color,
+  //     borderRadius: `${borderRadius}px`,
+  //   };
+  // }
 
-  ngOnDestroy() {}
+  // applyChanges(): void {
+  //   this.setBoxStyles(
+  //     this.boxForm.get('size').value,
+  //     this.boxForm.get('backgroundColor').value,
+  //     this.boxForm.get('textColor').value,
+  //     this.boxForm.get('borderRadius').value
+  //   );
+  // }
+
+  ngOnDestroy(): void {}
 }
